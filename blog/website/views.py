31d01870aa44d873fe501eab4fe4f65014529488
Usiel F. Lopes Jr <ufljr.1997@gmail.com>
2020-05-21 14:30:53 -0300

    Testanto commandos Template

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_blog(request):
+    listaH = [
+        'C',
+        'C++',
+        'Java',
+        'Python',
+        'JS',
+        'TS',
+        'Haskell',
+        'Ionic',
+        'Django',
+    ]
+    data = {'name': 'Curso Django',
+            'listaHabilidades': listaH, 'coisa': 'COISAAAH'}
+    return render(request, 'index.html', data)
